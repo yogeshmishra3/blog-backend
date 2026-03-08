d82ef9f9e586b481443069237ebdb111c2cef20e
Yogesh Mishra <132771057+yogeshmishra3@users.noreply.github.com>
2026-03-08 15:18:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import ConnectionDB from "./config/db.js"
+import ConnectionDB from "./config/db.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import blogRoutes from "./routes/blogRoutes.js";
@@ -21,38 +20,19 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
+
 app.use(express.json());
 
 ConnectionDB();
 
 /* Routes */
-
 app.use("/api/auth", authRoutes);
 app.use("/api/blogs", blogRoutes);
 app.use("/api/comments", commentRoutes);
 
-
-/* MongoDB Connection */
-
-// mongoose
-//     .connect(process.env.MONGO_URI)
-//     .then(() => {
-//         console.log("MongoDB Connected");
-//     })
-//     .catch((err) => {
-//         console.error(err);
-//     });
-
 /* Health Route */
-
 app.get("/", (req, res) => {
     res.send("Blog API Running");
 });
 
-/* Server Start */
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+export default app;
